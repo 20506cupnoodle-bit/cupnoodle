44f0a310f7621db7db5bb61089f88eb32460a6e8
cupnoodle <11335094@dcsh.tp.edu.tw>
2025-11-28 12:03:42 +0800

    網站連結跳轉

--- default/urls.py
+++ default/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from.views import poll_list
+from.views import poll_list, pollist, pollview, pollvote
 
 urlpatterns = [
-    path("poll/", poll_list),
+    path("", poll_list),
+    path("list", pollist.as_view(), name='list'),
+    path("<int:pk>/", pollview.as_view(), name='pollview'),
+    path('<int:pk>/vote/', pollvote.as_view(), name='pollvote'),
 ]
--- default/views.py
+++ default/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from .models import Poll, Option
-# Create your views here.
+from django.views.generic import ListView, DetailView, RedirectView
+
+
 def poll_list(request):
     polls = Poll.objects.all() #全部的紀錄我都要
     return render(request, "default/list.html", {'poll_list': polls, 'msg': 'hello!!!'})
 # render 繪製(資料請求葉面範本, 字典)
+
+class pollist(ListView):
+    model = Poll #從poll資料模型裡面要資料
+
+    # 這具函式直接繼承，不用再打一次了
+    #應用程式名稱/資料模型_list.html
+    #會去default 下面找資料範本
+    #default/poll_list.html
+class pollview(DetailView):
+    model = Poll
+    #default/poll_detail.html
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        # 塞選的方法
+        ctx["option_list"] = Option.objects.filter(poll_id=self.object.id)
+        return ctx
+
+class pollvote(RedirectView):
+    pass
